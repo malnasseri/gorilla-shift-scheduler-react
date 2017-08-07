8301c93769464ca87998d02a63931b80b36adf91
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2017-08-07 10:56:33 -0400

    changed to mongodb uri

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,14 @@
  /*Mongo Database*/
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-let MONGO_URL;
+let MONGODB_URI;
 
 if (process.env.MONGODB_URI) {
-	MONGO_URL = process.env.MONGODB_URL
+	MONGODB_URI = process.env.MONGODB_URI
 	mongoose.connect(process.env.MONGODB_URI)
 } else {
-	MONGO_URL = 'mongodb://localhost/gorilla-test2'
-	mongoose.connect(MONGO_URL) // local mongo url
+	MONGODB_URI = 'mongodb://localhost/gorilla-test2'
+	mongoose.connect(MONGODB_URI) // local mongo url
 }
 // should mongoose.connection be put in the call back of mongoose.connect???
 const db = mongoose.connection
@@ -17,7 +17,7 @@ db.on('error', err => {
 })
 db.once('open', () => {
 	console.log(
-		`You have successfully connected to your mongo database: ${MONGO_URL}`
+		`You have successfully connected to your mongo database: ${MONGODB_URI}`
 	)
 })
 
